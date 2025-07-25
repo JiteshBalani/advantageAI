62e2ddd36adbb944fa7ce86ef874a190bca9482e
JiteshBalani <balani.jitesh911@gmail.com>
2025-07-26 02:57:20 +0530

    Debugging deployment issue.

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,9 @@ import { createRoot } from 'react-dom/client'
 import '@ant-design/v5-patch-for-react-19';
 import './index.css'
 import App from './App.jsx'
-import store from './app/store'
-import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </StrictMode>,
 )
