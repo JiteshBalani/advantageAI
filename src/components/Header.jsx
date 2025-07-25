4db5935629436561d347bef116d4f44e9ce3d894
JiteshBalani <balani.jitesh911@gmail.com>
2025-07-26 04:09:45 +0530

    Updated README.md file and added responsive design to Header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import Logo from '../../Logo.png'
 import { Link } from 'react-router-dom'
+import { HomeOutlined, UnorderedListOutlined } from '@ant-design/icons'
 
 const Header = () => {
   return (
-    // <div className='flex justify-between items-center h-[70px]'>
-    <div className='fixed top-0 left-0 w-full z-50 bg-white flex justify-between items-center h-[60px] px-4'>
-      <div>
-        <Link to='/' className='flex justify-center items-center text-2xl space-x-1 font-semibold text-[#D3145A]'>
-          <img src={Logo} width='30px' alt='Borrowland Logo' />
-          <span>Borrowland</span>
+    <div className='fixed top-0 left-0 w-full z-50 bg-white flex justify-between items-center h-[60px] px-4 shadow-md'>
+      {/* Logo Section */}
+      <Link to='/' className='flex items-center space-x-2 text-2xl font-semibold text-[#D3145A]'>
+        <img src={Logo} width='30px' alt='Borrowland Logo' />
+        {/* Show 'Borrowland' text only on screens ≥ sm */}
+        <span className='hidden sm:block'>Borrowland</span>
+      </Link>
+
+      {/* Navigation Icons */}
+      <div className='flex items-center gap-6 text-[#D3145A] text-lg font-semibold'>
+
+        {/* Desktop Text Labels */}
+        <Link to='/' className='hidden sm:flex items-center gap-1'>
+          <HomeOutlined />
+          <span>Home</span>
         </Link>
-      </div>
-      <div className='flex justify-between items-center cursor-pointer gap-8 text-lg font-semibold text-[#D3145A] '>
-        <Link to='/'><div>Home</div></Link>
-        <Link to='/my-requests'><div>My-Requests</div></Link>
+
+        <Link to='/my-requests' className='hidden sm:flex items-center gap-1'>
+          <UnorderedListOutlined />
+          <span>My Requests</span>
+        </Link>
+
+        {/* Mobile Only Icons */}
+        <Link to='/' className='block sm:hidden'>
+          <HomeOutlined className='text-2xl' />
+        </Link>
+
+        <Link to='/my-requests' className='block sm:hidden'>
+          <UnorderedListOutlined className='text-2xl' />
+        </Link>
+
       </div>
     </div>
   )
